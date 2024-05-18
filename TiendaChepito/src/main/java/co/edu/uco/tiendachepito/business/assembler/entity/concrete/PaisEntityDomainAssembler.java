61652efc42039fab85ba1b9a3190df24c1a7cbb8
kevinmurcia636 <kevinmurciamo636@gmail.com>
2024-05-18 14:30:30 -0500

    Assembler

---
+++ TiendaChepito/src/main/java/co/edu/uco/tiendachepito/business/assembler/entity/concrete/PaisEntityDomainAssembler.java
@@ -0,0 +1,29 @@
+package co.edu.uco.tiendachepito.business.assembler.entity.concrete;
+
+import co.edu.uco.tiendachepito.business.assembler.entity.EntityDomainAssembler;
+import co.edu.uco.tiendachepito.business.domain.PaisDomain;
+import co.edu.uco.tiendachepito.crosscutting.helpers.ObjectHelper;
+import co.edu.uco.tiendachepito.entity.PaisEntity;
+
+public class PaisEntityDomainAssembler implements EntityDomainAssembler<PaisDomain, PaisEntity>{
+
+	@Override
+	public final PaisDomain ensamblarDominio(final PaisEntity entidad) {
+		var paisEntityTmp = ObjectHelper.getObjectHelper().getDefault(entidad, PaisEntity.build(0));
+		return PaisDomain.crear(paisEntityTmp.getId(), paisEntityTmp.getNombre());
+	}
+	
+	@Override
+	public final PaisEntity ensamblarEntidad(final PaisDomain dominio) {
+		var paisDomainTmp = ObjectHelper.getObjectHelper().getDefault(dominio, PaisDomain.crear());
+		return PaisEntity.build(paisDomainTmp.getId(), paisDomainTmp.getNombre());
+	}
+	
+	@Override
+	public final List<PaisDomain> ensamblarListaDominios(final List<PaisEntity> listaEntidades) {
+		var listaEntidadesTmp =  ObjectHelper.getObjectHelper().getDefault(listaEntidades, new);
+		var resultados = new ArrayList<PaisDomain>();
+		
+		for (PaisEntity paisEntity : listaEntidadesTmp)
+	}
+}
---
+++ TiendaChepito/src/main/java/co/edu/uco/tiendachepito/business/facade/concrete/ConsultarPaisesImpl.java
@@ -0,0 +1,22 @@
+package co.edu.uco.tiendachepito.business.facade.concrete;
+
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+public class ConsultarPaisesImpl implements ConsultarPaises {
+	private final DAOFactory factory;
+	
+	public ConsultarPaisesImpl(final DAOFactory factory) {
+		this.factory = factory;
+	}
+	
+	@Override
+	public final List<PaisDomain> ejecutar()
+=======
+public class ConsultarPaisesImpl implements{
+
+>>>>>>> Stashed changes
+=======
+public class ConsultarPaisesImpl implements{
+
+>>>>>>> Stashed changes
+}
